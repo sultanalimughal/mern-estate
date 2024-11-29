c919a0ec7eb6ae85bd8c18c6b09f907c9af6b89a
sultanalimughal <sultanalimughal01@gmail.com>
2024-11-29 22:30:44 +0500

    upload api, backend

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# mern-estate.github.io
+# mern-estate
+This is MERN js project for practice watching from YouTube.
+
+## 1. Install Dependencies
+In Mern-estate and client folder
+```
+npm install
+```
+
+## 2. Run App
+
+# Run in dev mode
+```
+npm run dev
+
---
+++ api/routes/auth.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  signin,
+  signup,
+  google,
+  signOut,
+} from "../controllers/auth.contoller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/google", google);
+router.get("/signout", signOut);
+
+export default router;
